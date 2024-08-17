36b4dd86ff3bb56fbb8c697c76effe2a5d829d89
EclAdler <155657032+SkittleDH@users.noreply.github.com>
2024-08-17 19:14:55 +0200

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function checkLogin() {
     if (username === "TicTacToe") {
         window.location.href = "https://htmlpreview.github.io/?https://github.com/SkittleDH/TicTacToe/blob/main/index.html";
     } else {
-        alert("Error\n\nInvalid Code");
+        document.getElementById('error-popup').classList.remove('hidden');
     }
+}
+
+function closePopup() {
+    document.getElementById('error-popup').classList.add('hidden');
 }

b3cac4b8d484242f9f3be3f1617dac6aced6f33c
EclAdler <155657032+SkittleDH@users.noreply.github.com>
2024-08-17 18:07:28 +0200

    script.js

--- script.js
+++ script.js
@@ -4,22 +4,6 @@ function checkLogin() {
     if (username === "TicTacToe") {
         window.location.href = "https://htmlpreview.github.io/?https://github.com/SkittleDH/TicTacToe/blob/main/index.html";
     } else {
-        // Show modal popup
-        const modal = document.getElementById("myModal");
-        modal.style.display = "block";
-    }
-}
-
-// Close the modal when the user clicks the "x"
-document.querySelector(".close").onclick = function() {
-    const modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
-
-// Close the modal if the user clicks anywhere outside of the modal content
-window.onclick = function(event) {
-    const modal = document.getElementById("myModal");
-    if (event.target === modal) {
-        modal.style.display = "none";
+        alert("Error\n\nInvalid Code");
     }
 }

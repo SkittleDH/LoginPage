ddfc3ad62166581ca51584b419fb8eb3b50e74fe
EclAdler <155657032+SkittleDH@users.noreply.github.com>
2024-08-19 20:23:43 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Redirect to TicTacToe page when correct input is entered
+// Redirect based on the username entered
 document.getElementById('submitBtn').addEventListener('click', function() {
     const username = document.getElementById('username').value;
 
-    // Check if the entered username is "TicTacToe"
     if (username === "TicTacToe") {
         window.location.href = "https://htmlpreview.github.io/?https://github.com/SkittleDH/TicTacToe/blob/main/index.html";
+    } else if (username === "Proxy") {
+        window.location.href = "https://funnyloginpage.yaha.pro/";
     } else {
         // Show error popup if the input is incorrect
         const errorPopup = document.getElementById('error-popup');
